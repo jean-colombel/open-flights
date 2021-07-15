1203f0985767263ac0dfaf32773b35814dc04ced
Jean Colombel <jeancolombel93@gmail.com>
2021-07-15 17:49:11 -0400

    Worked on the Airlines component

--- app/javascript/components/Airlines/airlines.jsx
+++ app/javascript/components/Airlines/airlines.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+
+import Airline from './airline'
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for our app.</div>
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    // Get all our airlines from the API
+    // Update airlines in our state
+    axios.get('/api/v1/airlines.json')
+      .then(response => {
+        setAirlines(response.data.data)
+      })
+      .catch(response => console.log (response))
+  }, [airlines.length  ])
+
+  const grid = airlines.map( item => {
+    return (
+      <Airline
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
+  })
+
+  return(
+    <div className="home">
+      <div className="head">
+        <h1>OpenFlights</h1>
+        <div className="subheader">
+          Honest, unbiased airline reviews.
+        </div>
+      </div>
+      <div className="grid">
+        <ul>{grid}</ul>
+      </div>
+    </div>
+  )
 }
 
 export default Airlines
