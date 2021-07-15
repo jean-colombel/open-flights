cbf38ff2d50276028abd56f9eb6586a65a5ec8ba
Jean Colombel <jeancolombel93@gmail.com>
2021-07-15 16:36:11 -0400

    Created the API

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,13 @@
 class Airline < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  before_create :slugify
+
+  def slugify
+    self.slug = self.name.parameterize
+  end
+
+  def avg_score
+    self.reviews.average(:score).round(2).to_f
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airline.destroy_all
+Review.destroy_all
+
+10.times do
+  Airline.create({
+    name: "The #{Faker::Hipster.word} #{Faker::Hipster.word}",
+    image_url: 'https://images.unsplash.com/photo-1571306603861-20c055ab2e5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+  })
+end
+
+Review.create([
+  {
+    title: 'Great airline',
+    description: 'I had a great flight',
+    score: 5,
+    airline: Airline.first
+  },
+  {
+    title: 'Bad airline',
+    description: 'Worst flight ever',
+    score: 1,
+    airline: Airline.first
+  }
+])
